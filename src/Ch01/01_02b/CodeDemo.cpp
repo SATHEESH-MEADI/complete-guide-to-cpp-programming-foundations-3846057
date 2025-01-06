8f4c9db57a8d3a27e4843a7e65a646e2cdd74685
SATHEESH MEADI <77147709+SATHEESH-MEADI@users.noreply.github.com>
2025-01-06 20:05:26 +0000

    Hello World executed

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,23 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+//Important: 
+
+//Case Sensitive 
+// cout means short for character out 
+
+#include <iostream>  //Preprocessing Directives 
+
+/*
+Every Statement in c++ ends with the semi-colon
+But we don't use the semicolon for the preprocessing directives
+
+*/
+
+
+int main(){
+    std::cout << "Hi There!" <<std::endl;
+    std::cout<<std::endl<<std::endl;
+    return 0;
+}
